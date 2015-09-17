54fbc52a020558b754a86eb6f70a73117b4b05cc
wseo88 <wseo88@gmail.com>
2015-09-17 13:53:19 -0700

    Added Bower packages

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 
 // set the static files location /public/img will be /img for users
+app.use('/libs', express.static(__dirname + '/bower_components'));
 app.use('/static', express.static(__dirname + '/public'));
 
 // routes ==================================================
